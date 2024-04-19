96f2f7f6f74b243582727d830472873ae2e3583a
Lea Mae Tortogo <159240740+lianna014@users.noreply.github.com>
2024-04-19 16:14:06 +0800

    Delete js script.js

--- js script.js
+++
@@ -1,6 +0,0 @@
-let menu_icon = document.querySelector("#menu_icon");
-let menu = document.querySelector(".nav_menu");
-
-menu_icon.addEventListener("click", function(){
-menu.classList.toggle("show");
-});
